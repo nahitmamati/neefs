28e54c54159464dcd9a8e0e895ec5e6bd2a37533
Alpemi52 <muftuoglualperen768@gmail.com>
2023-05-20 20:16:32 +0300

    .

--- lib/features/news/presentation/pages/home_page.dart
+++ lib/features/news/presentation/pages/home_page.dart
@@ -1,60 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:neefs/features/news/presentation/widgets/app_bar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: const Drawer(),
-      appBar: AppBar(
-        leading: Builder(
-          builder: (context) => IconButton(
-            icon: Container(
-                height: 45,
-                width: 45,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(50),
-                ),
-                child: const Icon(Icons.menu_rounded)),
-            onPressed: () => Scaffold.of(context).openDrawer(),
-          ),
+    return SafeArea(
+      child: Scaffold(
+        drawer: const Drawer(),
+        appBar: PreferredSize(
+          preferredSize: Size(double.infinity, AppBar().preferredSize.height),
+          child: const MyAppBar(),
         ),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.search),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.notifications_none),
-          ),
-          const SizedBox(
-            width: 15,
-          )
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home_outlined),
-            activeIcon: Icon(Icons.home),
-            label: "",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.confirmation_num_outlined),
-            activeIcon: Icon(Icons.confirmation_num),
-            label: "",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person_outline),
-            activeIcon: Icon(Icons.person),
-            label: "",
-          ),
-        ],
-      ),
-      body: SafeArea(
-        child: Column(
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined),
+              activeIcon: Icon(Icons.home),
+              label: "",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.confirmation_num_outlined),
+              activeIcon: Icon(Icons.confirmation_num),
+              label: "",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline),
+              activeIcon: Icon(Icons.person),
+              label: "",
+            ),
+          ],
+        ),
+        body: Column(
           children: [
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 12.0),
---
+++ lib/features/news/presentation/widgets/app_bar.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class MyAppBar extends StatelessWidget {
+  const MyAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leading: Builder(
+        builder: (context) => IconButton(
+          icon: Container(
+              height: 45,
+              width: 45,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50),
+              ),
+              child: const Icon(Icons.menu_rounded)),
+          onPressed: () => Scaffold.of(context).openDrawer(),
+        ),
+      ),
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.search),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.notifications_none),
+        ),
+        const SizedBox(
+          width: 15,
+        )
+      ],
+    );
+  }
+}
